5f9594bdd94541f88670f6292edb2699d0f4397c
Maya Lekhi <mlekhi@makenotion.com>
2026-02-28 20:45:13 -0800

    setting new browse only for new users / ppl w no chat history

--- src/app/browse/new/page.tsx
+++ src/app/browse/new/page.tsx
@@ -6,6 +6,9 @@ export default async function NewBrowsePage() {
   const session = await getSession();
   if (!session) redirect("/");
 
+  const conversations = await api.conversation.list();
+  if (conversations.length > 0) redirect("/browse");
+
   const conversation = await api.conversation.create({});
   redirect(`/browse/${conversation.id}`);
 }
